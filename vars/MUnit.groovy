ae6d77c586fff938f841330907662ba7a1ae9364
Ashwin Athrey Yellapantula <ashwinathrey93@gmail.com>
2019-05-22 16:06:30 -0400

    Update MUnit.groovy

--- vars/MUnit.groovy
+++ vars/MUnit.groovy
@@ -1,10 +1,4 @@
-import hudson.model.*
-import hudson.EnvVars
-import groovy.json.JsonSlurperClassic
-import groovy.json.JsonBuilder
-import groovy.json.JsonOutput
-import groovy.json.*
-import java.net.URL
+
 def coveragepercentage
 def testFunc(){
 
@@ -23,19 +17,4 @@ def testFunc(){
            currentBuild.result = 'FAILURE'
                 }
 }
-node {
-stage('build') {
-            script {
-			sh 'mvn clean package'
-         }
-}
-stage('Json coverage') {
 
-  testFunc()
-}
-stage('MUnit Test Report') {
-                script {
-              publishHTML(target:[allowMissing: false,alwaysLinkToLastBuild: true,keepAll: true,reportDir: 'target/site/munit/coverage',reportFiles: 'summary.html',reportName: 'MUnit Test Report',reportTitles: 'MUnit Test Coverage Report'])
-              }
-              }
-}
